d646810a5fadd9c834bd9d2e15684206adb44be4
ItsArias <felixarias2doc@gmail.com>
2026-06-04 16:48:28 -0400

    N

--- CS50W-Lecture2-Python/sequences.py
+++ CS50W-Lecture2-Python/sequences.py
@@ -1,3 +1,16 @@
 name = "Harry"
 
+#the number: (0), is going to print the letter number: (H)
 print(name[0])
+
+
+names = ["Harry", "Ron", "Hermaione"]
+
+#the number: (0), is going to print the first value: (Harry)
+print(names[0])
+
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
